fed98ee19fddb29bc174886ec1d44d7a7162370d
Alistair Phillips <alphillips101@gmail.com>
2017-05-29 16:29:30 +1000

    Add readme

--- README.md
+++ README.md
@@ -1,16 +1,66 @@
 # reference-data-selector
 
-[![Travis][build-badge]][build]
-[![npm package][npm-badge]][npm]
-[![Coveralls][coveralls-badge]][coveralls]
+Typeahead field to look up reference data.
 
-Describe reference-data-selector here.
+Specify the ref type to look up a reference data group.
 
-[build-badge]: https://img.shields.io/travis/user/repo/master.png?style=flat-square
-[build]: https://travis-ci.org/user/repo
+For example to look up countries from the `country` data group, pass in the type of `country`
 
-[npm-badge]: https://img.shields.io/npm/v/npm-package.png?style=flat-square
-[npm]: https://www.npmjs.org/package/npm-package
+```
+<ReferenceDataSelector
+    type="country"
+    ...
+```    
 
-[coveralls-badge]: https://img.shields.io/coveralls/user/repo/master.png?style=flat-square
-[coveralls]: https://coveralls.io/github/user/repo
+> Note this only works if it deployed under the same domain as the reference data service.
+
+> Or use a proxy. 
+
+## Usage
+
+### Install
+```
+npm i @react-ag-components/reference-data-selector --save
+```
+### Use in your project
+```
+import ReferenceDataSelector from '@react-ag-components/reference-data-selector'
+```
+
+```
+<ReferenceDataSelector
+  id="country-selector"
+  label="Country you wish to export to"
+  placeholder="Select country"
+  type="country"
+  onChange={this.onChange('country')}
+  value={this.state.country}
+/>
+```
+
+### Properties
+
+
+## Contributing
+
+Get the repository
+```
+git clone https://github.com/alphillips/reference-data-selector.git
+```
+
+Update dependencies
+```
+npm install
+```
+
+Run the project
+```
+npm start
+```
+
+### Deploy to npm
+#### Build
+`npm run build -- --copy-files`
+
+#### Publish
+`npm publish --access public`
--- src/index.js
+++ src/index.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
 
-export default React.createClass({
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+import Select from 'react-select'
+
+import 'react-select/dist/react-select.css'
+import './ref-data-selector.css'
+
+class RefDataSelector extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      options: []
+    }
+
+    this.linkStyle = {
+      maxWidth: props.maxWidth || "100%"
+    };
+
   }
-})
+
+  componentDidMount() {
+    let urlPrefix = '/clientportal/rs/resources/ref/'
+    fetch(urlPrefix + this.props.type, { credentials: 'same-origin' }).then(
+      response => {
+        if (response.status === 200) {
+          response.text().then(data => {
+            this.setState({
+              options: JSON.parse(data)
+            })
+          });
+        }
+      }
+    )
+  }
+
+  onChange = (val) => {
+    var value = val ? val.value : null
+    if (this.props.onChange) {
+      this.props.onChange(value)
+    }
+  }
+
+  render() {
+    const { id, label, value, error, placeholder } = this.props;
+
+    const errorClass = error ? 'hasError' : '';
+
+    return (
+      <div className={"text-group ref-data-selector " + errorClass} style={this.linkStyle}>
+        <label htmlFor={id}>{label}</label>
+        <Select
+          name="form-field-name"
+          options={this.state.options}
+          onChange={this.onChange}
+          value={value}
+          placeholder={placeholder}
+          inputProps={{ id: id, className: 'hideError' }}
+        />
+        <span role="alert" aria-live="polite" className={errorClass}>{error}</span>
+      </div>
+    )
+  }
+}
+
+export default RefDataSelector
